81139a84ac265fc3abf5842ddfb4fa2d92b8cbbb
Usama Rahman <rahmanusama390@gmail.com>
2023-11-02 06:12:37 +0600

    Start

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -20,7 +20,14 @@ const Navbar = () => {
     
     }}>
         <Link to='/'>
-            <img src={Logo} alt="logo" style={{width: '48px', height: '48px', margin:'0 20px'}} />
+            <img 
+            src={Logo} 
+            alt="logo" 
+            style={{
+                width: '48px', 
+                height: '48px', 
+                margin:'0 20px'
+            }} />
         </Link>
         <Stack
             direction='row'
